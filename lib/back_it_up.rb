008cd9433f4b05d799abd4c1052a54b7cbef0f51
Robin Spainhour <robin@robinspainhour.com>
2009-05-27 06:42:42 +0200

    Started zipping

--- lib/back_it_up.rb
+++ lib/back_it_up.rb
@@ -1,2 +1,3 @@
 require 'back_it_up/runner'
 require 'back_it_up/config'
+require 'back_it_up/file_packager'
--- lib/back_it_up/config.rb
+++ lib/back_it_up/config.rb
@@ -19,6 +19,15 @@ module BackItUp
       yield
     end
     
+    # Full path and name of the package file to create.
+    # A timestamp will be appended to the filename and the extension is 
+    # determined by the packager used.
+    #
+    #   # Using the zip packager with a destination file setting:
+    #   /var/backup/mybackup 
+    #
+    #   # ...then the final result might be:
+    #   /var/backup/mybackup_200900527_053032.zip
     def destination_file(filename) 
       raise "Invalid destination file." if File.directory?(filename)
       @dest_filename = filename
