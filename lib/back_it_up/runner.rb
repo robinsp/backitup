be2ff0f6a981469aa661d14724532462a8ed4bb3
Robin Spainhour <robin@robinspainhour.com>
2009-05-27 07:33:02 +0200

    Runner now executes packaging

--- lib/back_it_up/runner.rb
+++ lib/back_it_up/runner.rb
@@ -4,8 +4,8 @@ module BackItUp
       raise "Couldn't find file #{filename}" unless File.exists?(filename)
       
       file = File.open(filename) 
-      Config.new(file)
-    
+      config = Config.new(file)
+      puts "Wrote backup to: #{ FilePackager.new(config).package }"
     end
   end
 end
--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -7,15 +7,44 @@ describe BackItUp::Runner do
     lambda {BackItUp::Runner.new(name_of_invalid_file)}.should raise_error("Couldn't find file #{name_of_invalid_file}")
   end
 
-  it "should create Config with file handle" do 
-    File.stubs(:exists?).returns(true)
+  describe "with valid file" do 
+    before do 
+      @package_result_filename = "package_result_filename"
+      
+      File.stubs(:exists?).returns(true)
+      @filename = "a-file-name"
+      
+      @file = mock('fake file handle')
+      File.stubs(:open).with(@filename).returns(@file)
+      
+      @mock_config = mock('mock-config')
+      BackItUp::Config.stubs(:new).returns(@mock_config)
+      
+      @mock_packager
+      @mock_packager.stubs(:package).returns(@package_result_filename)
+      BackItUp::FilePackager.stubs(:new).returns(@mock_packager)
+      
+      
+    end
+    it "should create Config with file handle" do 
+      BackItUp::Config.expects(:new).with( @file ).returns(@mock_config)
+      BackItUp::Runner.new(@filename)
+    end
     
-    filename = "a-file-name"
-    file = mock('fake file handle')
-    File.expects(:open).with(filename).returns(file)
-    BackItUp::Config.expects(:new).with( file )
+    it "should create Packager with Config" do 
+      BackItUp::FilePackager.expects(:new).with(@mock_config)
+      BackItUp::Runner.new(@filename)
+    end
     
-    BackItUp::Runner.new(filename)
+    it "should start packaging" do 
+      @mock_packager.expects(:package)
+      BackItUp::Runner.new(@filename)
+    end
+    
+    it "should print name of packaged file" do 
+      BackItUp::Runner.any_instance.expects(:puts).with {|s| s =~ /#{@package_result_filename}/ }
+      BackItUp::Runner.new(@filename)
+    end
     
   end
   
