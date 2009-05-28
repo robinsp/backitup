220ac9a921e088bdcdf69f09f812ac32781e5788
Robin Spainhour <robin@robinspainhour.com>
2009-05-28 16:04:56 +0200

    Moved methods that are usable in end user's backup script.

--- lib/back_it_up/config.rb
+++ lib/back_it_up/config.rb
@@ -1,5 +1,47 @@
 module BackItUp
+  module ScriptDSL 
+    
+    def backup
+      yield
+    end
+    
+    def file(filename = nil)
+      unless (filename  == "" || filename == nil)
+        if File.exists?(filename)
+          if File.directory? filename
+            @dirs << filename
+          else
+            @files << filename
+          end
+              
+        else 
+          puts "WARNING: File #{filename} could not be found"    
+        end
+      end
+      
+    end
+    
+    alias_method :dir, :file
+    
+    # Full path and name of the package file to create.
+    # A timestamp will be appended to the filename and the extension is 
+    # determined by the packager used.
+    #
+    #   # Using the zip packager with a destination file setting:
+    #   /var/backup/mybackup 
+    #
+    #   # ...then the final result might be:
+    #   /var/backup/mybackup_200900527_053032.zip
+    def destination_file(filename) 
+      raise "Invalid destination file." if File.directory?(filename)
+      @dest_filename = filename
+    end
+    
+  end
+  
+  
   class Config
+    include ScriptDSL
     attr_reader :files, :dirs, :dest_filename
     
     def initialize(file_handle)
@@ -18,42 +60,5 @@ module BackItUp
     def each_file 
       FileList.new(@files, @dirs).files.each {|filename| yield filename }
     end
-    
-    def backup
-      yield
-    end
-    
-    # Full path and name of the package file to create.
-    # A timestamp will be appended to the filename and the extension is 
-    # determined by the packager used.
-    #
-    #   # Using the zip packager with a destination file setting:
-    #   /var/backup/mybackup 
-    #
-    #   # ...then the final result might be:
-    #   /var/backup/mybackup_200900527_053032.zip
-    def destination_file(filename) 
-      raise "Invalid destination file." if File.directory?(filename)
-      @dest_filename = filename
-    end
-    
-    def file(filename = nil)
-
-      unless (filename  == "" || filename == nil)
-        if File.exists?(filename)
-          if File.directory? filename
-            @dirs << filename
-          else
-            @files << filename
-          end
-              
-        else 
-          puts "WARNING: File #{filename} could not be found"    
-        end
-      end
-      
-    end
-    
-    alias_method :dir, :file
   end  
 end
