356d949584de824549a18a15a17162b21f782f4c
Robin Spainhour <robin@robinspainhour.com>
2009-05-29 14:45:06 +0200

    Added blank? method

--- lib/back_it_up.rb
+++ lib/back_it_up.rb
@@ -1,3 +1,15 @@
+class NilClass 
+  def blank?
+    true
+  end
+end
+
+class String 
+  def blank?
+    (self == nil || self == "")
+  end
+end
+
 require 'back_it_up/runner'
 require 'back_it_up/config'
 require 'back_it_up/file_packager'
